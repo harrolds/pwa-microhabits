b0b659707c34f4d1facc154d9059833eb84fb3c6
Harrold <36454763+harrolds@users.noreply.github.com>
2025-11-25 09:49:21 +0100

    Delete src directory

--- src/firebase/firebase.db.ts
+++
@@ -1,6 +0,0 @@
-import { getFirestore, type Firestore } from 'firebase/firestore';
-
-import { getFirebaseApp } from './firebase.app';
-
-export const getFirebaseDb = (): Firestore => getFirestore(getFirebaseApp());
-
