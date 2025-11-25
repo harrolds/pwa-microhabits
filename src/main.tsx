021a375d606bd17faaab3c0a60440479dd63a731
Harrold <36454763+harrolds@users.noreply.github.com>
2025-11-25 12:30:53 +0100

    Changed main tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Shell from "./app/Shell/Shell";
+import { Shell } from './app/Shell/Shell';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
