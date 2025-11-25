0a40ca0e9f697f636d08b2a3d7ed85a0d66ac6fb
Harrold <36454763+harrolds@users.noreply.github.com>
2025-11-25 17:01:07 +0100

    Updated sheetStore file

--- src/app/layout/sheets/sheetStore.ts
+++ src/app/layout/sheets/sheetStore.ts
@@ -1,8 +1,13 @@
-
 import { create } from 'zustand';
 
-interface SheetState { id: string|null; open:(id:string)=>void; close:()=>void; }
+interface SheetState {
+  id: string | null;
+  open: (id: string) => void;
+  close: () => void;
+}
 
-export const useSheetHost = create<SheetState>((set)=>({
-  id:null, open:(id)=>set({id}), close:()=>set({id:null}})
-));
+export const useSheetHost = create<SheetState>((set) => ({
+  id: null,
+  open: (id) => set({ id }),
+  close: () => set({ id: null }),
+}));
